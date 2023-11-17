69a4052db253d68d7bc9ffb5f438707b691b4a28
mogan1999 <848137001@qq.com>
2023-11-17 15:50:19 +0800

    Initial commit

---
+++ hkgetdata20230422.sh
@@ -0,0 +1,222 @@
+#!/bin/bash
+source /usr/local/anaconda3/bin/activate tieba
+
+cd /home/raoziyang/Tieba_Spider
+
+python modify_config.py 香港城市大学;
+scrapy run 香港城市大学 香港城市大学data -p 1 4;
+echo current task finished at:
+date
+echo "waiting 7263s for next task......"
+sleep 7263
+python modify_config.py 香港中文大学;
+scrapy run 香港中文大学 香港中文大学data -p 1 4;
+echo current task finished at:
+date
+echo "waiting 7300s for next task......"
+sleep 7300
+python modify_config.py 香港科技大学;
+scrapy run 香港科技大学 香港科技大学data -p 1 4;
+echo current task finished at:
+date
+echo "waiting 7221s for next task......"
+sleep 7221
+python modify_config.py 香港大学;
+scrapy run 香港大学 香港大学data -p 1 4;
+echo current task finished at:
+date
+echo "waiting 7267s for next task......"
+sleep 7267
+python modify_config.py 香港理工大学;
+scrapy run 香港理工大学 香港理工大学data -p 1 4;
+echo current task finished at:
+date
+echo "waiting 7234s for next task......"
+sleep 7234
+python modify_config.py 香港浸会大学;
+scrapy run 香港浸会大学 香港浸会大学data -p 1 4;
+echo current task finished at:
+date
+echo "waiting 7213s for next task......"
+sleep 7213
+python modify_config.py 岭南大学;
+scrapy run 岭南大学 岭南大学data -p 1 4;
+echo current task finished at:
+date
+echo "waiting 7238s for next task......"
+sleep 7238
+python modify_config.py 香港教育学院;
+scrapy run 香港教育学院 香港教育学院data -p 1 4;
+echo current task finished at:
+date
+echo "waiting 7270s for next task......"
+sleep 7270
+python modify_config.py 香港歌德学院;
+scrapy run 香港歌德学院 香港歌德学院data -p 1 4;
+echo current task finished at:
+date
+echo "waiting 7225s for next task......"
+sleep 7225
+python modify_config.py 香港公开大学;
+scrapy run 香港公开大学 香港公开大学data -p 1 4;
+echo current task finished at:
+date
+echo "waiting 7279s for next task......"
+sleep 7279
+python modify_config.py 香港树仁大学;
+scrapy run 香港树仁大学 香港树仁大学data -p 1 4;
+echo current task finished at:
+date
+echo "waiting 7260s for next task......"
+sleep 7260
+python modify_config.py 香港演艺学院;
+scrapy run 香港演艺学院 香港演艺学院data -p 1 4;
+echo current task finished at:
+date
+echo "waiting 7210s for next task......"
+sleep 7210
+python modify_config.py 珠海学院;
+scrapy run 珠海学院 珠海学院data -p 1 4;
+echo current task finished at:
+date
+echo "waiting 7293s for next task......"
+sleep 7293
+python modify_config.py 香港高等科技教育学院;
+scrapy run 香港高等科技教育学院 香港高等科技教育学院data -p 1 4;
+echo current task finished at:
+date
+echo "waiting 7238s for next task......"
+sleep 7238
+python modify_config.py 明德学院;
+scrapy run 明德学院 明德学院data -p 1 4;
+echo current task finished at:
+date
+echo "waiting 7212s for next task......"
+sleep 7212
+python modify_config.py 恒生管理学院;
+scrapy run 恒生管理学院 恒生管理学院data -p 1 4;
+echo current task finished at:
+date
+echo "waiting 7247s for next task......"
+sleep 7247
+python modify_config.py 明爱学院;
+scrapy run 明爱学院 明爱学院data -p 1 4;
+echo current task finished at:
+date
+echo "waiting 7258s for next task......"
+sleep 7258
+python modify_config.py 东华学院;
+scrapy run 东华学院 东华学院data -p 1 4;
+echo current task finished at:
+date
+echo "waiting 7208s for next task......"
+sleep 7208
+python modify_config.py 香港城市大学;
+scrapy run 香港城市大学 香港城市大学data -p 1 4;
+echo current task finished at:
+date
+echo "waiting 7201s for next task......"
+sleep 7201
+python modify_config.py 香港中文大学;
+scrapy run 香港中文大学 香港中文大学data -p 1 4;
+echo current task finished at:
+date
+echo "waiting 7275s for next task......"
+sleep 7275
+python modify_config.py 香港科技大学;
+scrapy run 香港科技大学 香港科技大学data -p 1 4;
+echo current task finished at:
+date
+echo "waiting 7234s for next task......"
+sleep 7234
+python modify_config.py 香港大学;
+scrapy run 香港大学 香港大学data -p 1 4;
+echo current task finished at:
+date
+echo "waiting 7258s for next task......"
+sleep 7258
+python modify_config.py 香港理工大学;
+scrapy run 香港理工大学 香港理工大学data -p 1 4;
+echo current task finished at:
+date
+echo "waiting 7237s for next task......"
+sleep 7237
+python modify_config.py 香港浸会大学;
+scrapy run 香港浸会大学 香港浸会大学data -p 1 4;
+echo current task finished at:
+date
+echo "waiting 7294s for next task......"
+sleep 7294
+python modify_config.py 岭南大学;
+scrapy run 岭南大学 岭南大学data -p 1 4;
+echo current task finished at:
+date
+echo "waiting 7216s for next task......"
+sleep 7216
+python modify_config.py 香港教育学院;
+scrapy run 香港教育学院 香港教育学院data -p 1 4;
+echo current task finished at:
+date
+echo "waiting 7226s for next task......"
+sleep 7226
+python modify_config.py 香港歌德学院;
+scrapy run 香港歌德学院 香港歌德学院data -p 1 4;
+echo current task finished at:
+date
+echo "waiting 7211s for next task......"
+sleep 7211
+python modify_config.py 香港公开大学;
+scrapy run 香港公开大学 香港公开大学data -p 1 4;
+echo current task finished at:
+date
+echo "waiting 7300s for next task......"
+sleep 7300
+python modify_config.py 香港树仁大学;
+scrapy run 香港树仁大学 香港树仁大学data -p 1 4;
+echo current task finished at:
+date
+echo "waiting 7251s for next task......"
+sleep 7251
+python modify_config.py 香港演艺学院;
+scrapy run 香港演艺学院 香港演艺学院data -p 1 4;
+echo current task finished at:
+date
+echo "waiting 7300s for next task......"
+sleep 7300
+python modify_config.py 珠海学院;
+scrapy run 珠海学院 珠海学院data -p 1 4;
+echo current task finished at:
+date
+echo "waiting 7250s for next task......"
+sleep 7250
+python modify_config.py 香港高等科技教育学院;
+scrapy run 香港高等科技教育学院 香港高等科技教育学院data -p 1 4;
+echo current task finished at:
+date
+echo "waiting 7201s for next task......"
+sleep 7201
+python modify_config.py 明德学院;
+scrapy run 明德学院 明德学院data -p 1 4;
+echo current task finished at:
+date
+echo "waiting 7211s for next task......"
+sleep 7211
+python modify_config.py 恒生管理学院;
+scrapy run 恒生管理学院 恒生管理学院data -p 1 4;
+echo current task finished at:
+date
+echo "waiting 7247s for next task......"
+sleep 7247
+python modify_config.py 明爱学院;
+scrapy run 明爱学院 明爱学院data -p 1 4;
+echo current task finished at:
+date
+echo "waiting 7228s for next task......"
+sleep 7228
+python modify_config.py 东华学院;
+scrapy run 东华学院 东华学院data -p 1 4;
+echo current task finished at:
+date
+echo "waiting 7264s for next task......"
+sleep 7264
+echo "all done"
